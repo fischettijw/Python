782e7d9121e3baa26f531394e66595607d22aeee
Joseph Fischetti <fischettijw@gmail.com>
2021-04-28 11:59:25 -0400

    update

--- testTurtleGraphics.py
+++ testTurtleGraphics.py
@@ -5,20 +5,19 @@ ts = turtle.Screen()
 ts.title("Joseph Fischetti")
 ts.bgcolor("yellow")
 t = turtle.Turtle()
+tt = turtle.Turtle()
 
 t.pendown()
 t.pencolor("cyan")
 t.width(5)
 t.speed(10)
 
+
 for i in range(4):
     t.forward(100)
     t.right(90)
 t.hideturtle()
 
+
 ts.exitonclick()
 # ts.mainloop( )
-
-
-
-
