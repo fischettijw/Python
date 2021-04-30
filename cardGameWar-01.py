d84d9240610ddbb8311993b3a33eebf5c6a93837
Joseph Fischetti <fischettijw@gmail.com>
2021-04-30 07:12:37 -0400

    added cardGameWar-02.py

--- cardGameWar-01.py
+++ cardGameWar-01.py
@@ -8,7 +8,7 @@ def list_cards(lst):
 
 
 def wins():
-    print('='*28)
+    print('='*48)
     print(f'Player 1 Wins": {wins_player1}')
     print(f'Player 2 Wins": {wins_player2}')
     print(f'Computer Wins": {wins_computer}')
@@ -16,15 +16,13 @@ def wins():
 
 total_cards_in_deck = 52
 
-# player1 = []
-# player2 = []
 # deck = list(range(1, 14))+list(range(1, 14)) + list(range(1, 14))+list(range(1, 14))
 deck = list(range(1, total_cards_in_deck+1))
-cards_in_deck = len(deck)
+total_cards_in_deck = len(deck)
 random.shuffle(deck)
 
-player1 = deck[:(cards_in_deck//2)]
-player2 = deck[(cards_in_deck//2):]
+player1 = deck[:(total_cards_in_deck//2)]
+player2 = deck[(total_cards_in_deck//2):]
 # list_cards(player1)
 # list_cards(player2)
 
@@ -33,10 +31,12 @@ wins_player2 = 0
 wins_computer = 0
 
 for i in range(total_cards_in_deck//2):
-    print(f'Player 1: {player1[i]}   Player 2: {player2[i]}')
+    print(f'Player 1: {player1[i]} \t Player 2: {player2[i]}', end='')
     if player1[i] > player2[i]:
         wins_player1 += 1
+        print(f' \t-  Player 1 WINS')
     else:
         wins_player2 += 1
+        print(f' \t-  Player 2 WINS')
 
 wins()
---
+++ cardGameWar-02.py
@@ -0,0 +1,47 @@
+import random
+
+
+def list_cards(lst):
+    for itm in lst:
+        print(itm)
+    print('='*28)
+
+
+def wins():
+    print('='*48)
+    print(f'Player 1 Wins": {wins_player1}')
+    print(f'Player 2 Wins": {wins_player2}')
+    # print(f'Computer Wins": {wins_computer}')
+    print(wins1)
+    print(wins2)
+
+
+total_cards_in_deck = 52
+
+# deck = list(range(1, 14))+list(range(1, 14)) + list(range(1, 14))+list(range(1, 14))
+deck = list(range(1, total_cards_in_deck+1))
+total_cards_in_deck = len(deck)
+random.shuffle(deck)
+
+player1 = deck[:(total_cards_in_deck//2)]
+player2 = deck[(total_cards_in_deck//2):]
+
+wins_player1 = 0
+wins_player2 = 0
+wins_computer = 0
+
+wins1 = []
+wins2 = []
+for i in range(total_cards_in_deck//2):
+    print(f'Player 1: {player1[i]} \t Player 2: {player2[i]}', end='')
+    if player1[i] > player2[i]:
+        wins_player1 += 1
+        print(f' \t-  Player 1 WINS')
+        wins1.append(player1[i])
+    else:
+        wins_player2 += 1
+        print(f' \t-  Player 2 WINS')
+        wins2.append(player1[i])
+
+
+wins()
